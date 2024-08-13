3a7ad7edf4a430e2280ef3b3e3e00ee4315967d7
JLennyM <lennyjohn399@gmail.com>
2024-08-13 17:36:07 +0300

    the first pull

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import LoginPage from './pages/LoginPage/LoginPage'; // Updated import with proper casing
+import LoginPage from './pages/LoginPage/LoginPage'
 import SessionsPage from './pages/SessionsPage/SessionsPage'; // Import SessionsPage
 import CartPage from './pages/CartPage/CartPage'; // Import CartPage
 import { CartProvider } from './context/CartContext'; // Import CartProvider
 import '@fortawesome/fontawesome-free/css/all.min.css'; // Import FontAwesome
 
+
+
 const App = () => {
     return (
-        <div>
-          <LoginPage/>  
-        </div>
-            //<CartProvider>
-           // <Router>
-                //<Routes>
-                   // <Route path="/" element={<SessionsPage />} /> {/* Route for sessions page */}
-                    //<Route path="/cart" element={<CartPage />} /> {/* Route for cart page */}
-                   // <Route path="/login" element={<LoginPage />} /> {/* Route for login page */}
-               // </Routes>
-            //</Router>
-        //</CartProvider>
+       
+            <CartProvider>
+           <Router>
+                <Routes>
+                <Route path="/login" element={<LoginPage />} /> {/* Route for login page */}
+                <Route path="/" element={<SessionsPage />} /> {/* Route for sessions page */}
+                    <Route path="/cart" element={<CartPage />} /> {/* Route for cart page */}
+                    </Routes>
+            </Router>
+        </CartProvider>
     );
 };
 
